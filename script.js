93705557046911dbe2540dd0df2df9c9365a4471
Woonbeam Post <yulianaanisiforova@gmail.com>
2025-01-13 17:01:46 +0300

    created a gift card

---
+++ script.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector('.openButton')
+let showingBox = document.querySelector('.upperBox')
+
+btn.onclick = () => {
+    console.log('hehe')
+    showingBox.style = 'display: flex'
+}
+
