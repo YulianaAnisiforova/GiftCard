8132c6bbd89e4074e54ba5bc460045dc8473f337
Woonbeam Post <yulianaanisiforova@gmail.com>
2025-01-15 12:53:42 +0300

    added a close button

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-let btn = document.querySelector('.openButton')
+let openBtn = document.querySelector('.openButton')
 let showingBox = document.querySelector('.upperBox')
+let closeBtn = document.querySelector('.closeButton')
+let isShown = false;
 
-btn.onclick = () => {
-    console.log('hehe')
-    showingBox.style = 'display: flex'
+openBtn.onclick = () => {
+    if (isShown == false) {
+        showingBox.style = 'display: flex'
+        isShown = true
+    }
+    else 
+        showingBox.style = 'display: none'
+
+    if (openBtn.innerHTML == 'Open me') openBtn.innerHTML = 'Close me'
+    else openBtn.innerHTML = 'Open me'
 }
 
+closeBtn.onclick = () => {
+    showingBox.style = 'display: none'
+}
